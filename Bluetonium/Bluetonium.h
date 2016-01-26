3084dbc0a8e20dd626ae96ecd5d587188ede615b
Mathieu Monney <mathieu.monney@vidinoti.com>
2016-01-26 08:37:49 +0100

    Merged xcode example and framework in a single xcode project to add carthage compatibility

---
+++ Bluetonium/Bluetonium.h
@@ -0,0 +1,19 @@
+//
+//  Bluetonium.h
+//  Bluetonium
+//
+//  Created by Mathieu Monney on 26.01.16.
+//  Copyright © 2016 E-Sites. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bluetonium.
+FOUNDATION_EXPORT double BluetoniumVersionNumber;
+
+//! Project version string for Bluetonium.
+FOUNDATION_EXPORT const unsigned char BluetoniumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bluetonium/PublicHeader.h>
+
+
